5b48e9c651eed3c2e051bb79b98dc01693e6ef91
Ryan Atkinson <ryanatkn@gmail.com>
2013-10-05 23:20:11 -0400

    doc tweaks

--- backbone.giraffe.js
+++ backbone.giraffe.js
@@ -1198,7 +1198,7 @@
   * Additionally, rather than building anchor links and window locations manually,
   * you can build routes from app events and optional parameters with
   * `Giraffe.Router#getRoute`.
-  *     
+  *
   *     var myApp = new Giraffe.App;
   *     var myRouter = Giraffe.Router.extend({
   *       triggers: {
@@ -1636,7 +1636,7 @@
   * Features:
   *
   * - pulls option defaults from global, class, and instance `defaultOptions`
-  * - extends the object with all options
+  * - extends the object with all options minus `omittedOptions`
   * - defaults `obj.dispose` to the generic version in `Giraffe.defaultOptions`
   * - defaults `obj.app` to `Giraffe.app`
   * - binds `appEvents` if `appEvents` and `app` are defined
